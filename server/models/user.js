112ebba663a6d7f6e246e6e99980f24716b6d152
simulaterz <ronnakorn.suw@gmail.com>
2017-01-19 22:41:09 +0700

    Hash user password before saving

--- server/models/user.js
+++ server/models/user.js
@@ -2,6 +2,7 @@ const mongoose = require('mongoose');
 const validator = require('validator');
 const jwt = require('jsonwebtoken');
 const _ = require('lodash');
+const bcrypt = require('bcryptjs');
 
 var UserSchema = mongoose.Schema({
     email: {
@@ -53,22 +54,6 @@ UserSchema.methods.generateAuthToken = function () {
     });
 };
 
-// UserSchema.statics.findByToken = function (token) {
-//     var User = this;
-//     var decoded;
-//     try {
-//         decoded = jwt.verify(token, 'abc123');
-//     } catch (e) {
-//
-//     }
-//
-//     return User.findOne({
-//         '_id': decoded._id,
-//         'tokens.token': token,
-//         'tokens.access': 'auth'
-//     });
-// };
-
 UserSchema.statics.findByToken = function (token) {
     var User = this;
     var decoded;
@@ -88,6 +73,21 @@ UserSchema.statics.findByToken = function (token) {
         'tokens.access': 'auth'
     });
 };
+
+UserSchema.pre('save', function (next) {
+    var user = this;
+
+    if (user.isModified('password')) {
+        bcrypt.genSalt(10, (err, salt) => {
+            bcrypt.hash(user.password, salt, (err, hash) => {
+                user.password = hash;
+                next();
+            });
+        });
+    } else {
+        next();
+    }
+});
 
 var User = mongoose.model('User', UserSchema);
 
